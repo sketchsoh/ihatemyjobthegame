922caa838fc3f28cd81b3892e9260f9981c1198d
sketchsoh <sketchsoh@gmail.com>
2026-01-31 18:21:21 +0800

    Added Mask and BG

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameManager gameManager;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager = GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void CheckForInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //TODO: call flip hood in Game Manager
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //TODO: call bring down game in Game Manager
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //TODO: Do Quicktime Event
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //TODO: Do Quicktime Event
+        }
+    }
+}
