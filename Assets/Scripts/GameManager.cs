6ac260c70df0f4d12ffb6be76ae072002daf9865
sketchsoh <sketchsoh@gmail.com>
2026-01-31 18:30:07 +0800

    Yes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,15 +2,24 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public bool hoodMode;
+    public bool gameMode;
+    private Coroutine hoodToggleCoroutine;
+    private Coroutine gameToggleCoroutine;
     void Start()
     {
-        
+        hoodMode = false;
+        gameMode = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void HoodToggle()
+    {
+        hoodMode = !hoodMode;
     }
 }
